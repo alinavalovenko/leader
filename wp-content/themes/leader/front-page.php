83d61076b93b43e45f040779dccbd16d5327c07f
alinavalovenko <alina.valovenko@gmail.com>
2018-08-15 11:14:33 +0300

    first comit

---
+++ wp-content/themes/leader/front-page.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The front page template file
+ *
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ *
+ * @package sgo-2018
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<div>Front Page template. </div>
+
+<?php get_footer(); ?>
