2811b9f63906ba0b3ee809004f0739ec81eb4c47
alinavalovenko <alina.valovenko@gmail.com>
2018-08-21 10:37:32 +0300

    cleared theme structure

---
+++ wp-content/themes/valovenko/header.php
@@ -0,0 +1,7 @@
+<?php
+	/**
+	 * Created by PhpStorm.
+	 * User: user
+	 * Date: 8/21/2018
+	 * Time: 10:35 AM
+	 */
--- wp-content/themes/valovenko/index.php
+++ wp-content/themes/valovenko/index.php
@@ -2,6 +2,6 @@
 	/**
 	 * Created by PhpStorm.
 	 * User: user
-	 * Date: 8/15/2018
-	 * Time: 11:40 AM
+	 * Date: 8/21/2018
+	 * Time: 10:36 AM
 	 */
