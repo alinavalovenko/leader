1c5e033a4dbcf2da3bd29613e6a07bb6dd287f20
valovenko <alina.valovenko@gmail.com>
2018-11-09 23:16:10 +0200

    fill banner section

--- wp-content/themes/valovenko/partials/section-banner.php
+++ wp-content/themes/valovenko/partials/section-banner.php
@@ -1,39 +1,40 @@
+<?php $group_data = get_field( 'welcome_banner_options' ); ?>
 <div id="home" class="home-area parallax_img">
-	<div class="water-effect"></div>
-	<div class="container h-100">
-		<div class="row h-100 align-items-center">
-			<!--Title text-->
-			<div class="col-12 col-md-6 col-lg-6 home-content text-left">
-				<!-- Super heading -->
-				<h5 class="greet">Hello, my name is <strong>Sadiq Siddiqui</strong></h5>
-				<!-- Super heading ends!-->
-				<!-- heading text -->
-				<h1 class="skills cd-headline letters scale">
-					<span>I'm a</span>
-					<span class="cd-words-wrapper">
-                     <b class="is-visible">Developer</b>
-                     <b>Designer</b>
-                     <b>Freelancer</b>
-                     </span>
-				</h1>
-				<!-- heading text ends!-->
-				<!-- description -->
-				<p class="description">I'm a  Full-Stack Web Developer with extensive
-					experience for over 8 years. My expertise is to create and design
-					Websites, Apps, Mockups and many more...
-				</p>
-				<!-- description ends!-->
-				<!-- button-->
-				<div class="button-area">
-					<a class="btn scroll" href="#portfolio">Portfolio</a>
-					<a class="btn scroll button-scheme" href="#0">Resume</a>
-				</div>
-				<!-- button ends! -->
-			</div>
-			<!--Title text ends!-->
-		</div>
-		<div class="image-item row h-100 align-items-center">
-			<img src="<?php echo VL_IMAGES . 'big-profile.png'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>">
-		</div>
-	</div>
+    <div class="water-effect"></div>
+    <div class="container h-100">
+        <div class="row h-100 align-items-center">
+            <!--Title text-->
+            <div class="col-12 col-md-6 col-lg-6 home-content text-left">
+                <!-- Super heading -->
+                <h5 class="greet"><?php echo $group_data['title']; ?></h5>
+                <!-- Super heading ends!-->
+                <!-- heading text -->
+                <h1 class="skills cd-headline letters scale">
+					<?php echo $group_data['animated_text']; ?>
+                </h1>
+                <!-- heading text ends!-->
+                <!-- description -->
+                <p class="description"><?php echo $group_data['description']; ?>
+                </p>
+                <!-- description ends!-->
+                <!-- button-->
+                <div class="button-area">
+                    <a class="btn scroll"
+                       href="<?php echo $group_data['light_button_link']; ?>"><?php echo $group_data['light_button_text']; ?></a>
+                    <a class="btn scroll button-scheme"
+                       href="<?php echo $group_data['dark_button_link']; ?>"><?php echo $group_data['dark_button_text']; ?></a>
+                </div>
+                <!-- button ends! -->
+            </div>
+            <!--Title text ends!-->
+        </div>
+        <div class="image-item row h-100 align-items-center">
+			<?php if ( ! $group_data['front_banner'] ) { ?>
+                <img src="<?php echo VL_IMAGES . 'big-profile.png'; ?>"
+                     alt="<?php echo esc_html__( VL_DEFAULT_ALT, 'valovenko' ); ?>">
+			<?php } else { ?>
+				<?php echo wp_get_attachment_image( $group_data['front_banner']['ID'] ); ?>
+			<?php } ?>
+        </div>
+    </div>
 </div>
