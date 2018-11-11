ab47542e8cc1b1277f3f5eab8ca15d4f8b9d8033
valovenko <alina.valovenko@gmail.com>
2018-11-11 19:31:16 +0200

    service section added

--- wp-content/themes/valovenko/partials/section-portfolio.php
+++ wp-content/themes/valovenko/partials/section-portfolio.php
@@ -1,12 +1,11 @@
+<?php $group_data = get_field('portfolio_options'); ?>
 <div id="portfolio" class="single-section silver-bg portfolio-area">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12 col-lg-12">
 				<div class="section-heading">
-					<h2 class="section-title">Our Portfolio</h2>
-					<p class="section-description">Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-						Ratione!
-					</p>
+					<h2 class="section-title"><?php echo $group_data['section_title']; ?></h2>
+					<p class="section-description"><?php echo $group_data['section_subtitle']; ?></p>
 				</div>
 			</div>
 			<!--portfolio list-->
@@ -14,116 +13,35 @@
 				<div class="works-filter-wrap">
 					<div class="row">
 						<ul class="col-lg-12 col-md-12 list-inline works-filter">
-							<li class="list-inline-item tab-active" data-filter="*">All</li>
-							<li class="list-inline-item" data-filter=".brand">Brand</li>
-							<li class="list-inline-item" data-filter=".design">Design</li>
-							<li class="list-inline-item" data-filter=".photos">Photos</li>
+							<li class="list-inline-item tab-active" data-filter="*">Все</li>
+							<li class="list-inline-item" data-filter=".balkon">Балконы</li>
+							<li class="list-inline-item" data-filter=".mansarda">Мандарды</li>
+							<li class="list-inline-item" data-filter=".besedka">Беседки</li>
+							<li class="list-inline-item" data-filter=".bani">Бани/сауны</li>
 						</ul>
 					</div>
 				</div>
 				<div class="works-container row">
-					<div class="col-lg-4 col-sm-6 works-item brand">
-						<div class="item-overlay">
-							<div class="category">Brand</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-01.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Business Card</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-01.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-sm-6 works-item design">
-						<div class="item-overlay">
-							<div class="category">Design</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-02.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Anime Art</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-02.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-sm-6 works-item photos">
-						<div class="item-overlay">
-							<div class="category">Photos</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-03.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Business Card</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-03.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-sm-6 works-item photos">
-						<div class="item-overlay">
-							<div class="category">Photos</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-04.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Business Card</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-04.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-sm-6 works-item brand">
-						<div class="item-overlay">
-							<div class="category">Brand</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-05.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Business Card</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-05.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="col-lg-4 col-sm-6 works-item photos">
-						<div class="item-overlay">
-							<div class="category">Photos</div>
-							<img src="<?php echo VL_IMAGES . 'portfolio-06.jpg'; ?>" alt="<?php echo esc_html__(VL_DEFAULT_ALT, 'valovenko'); ?>"/>
-							<div class="overlay-content">
-								<h6 class="overlay-title">Business Card</h6>
-								<div class="overlay-icons">
-									<a href="images/portfolio-06.jpg" data-lity>
-										<span class="icon-eye"></span>
-									</a>
-									<a href="#0">
-										<span class="icon-link"></span>
-									</a>
-								</div>
-							</div>
-						</div>
-					</div>
+
+					<?php foreach ($group_data['portfolio_items'] as $item){ ?>
+                        <div class="col-lg-4 col-sm-6 works-item <?php $item['category_filter'] ?>">
+                            <div class="item-overlay">
+                                <div class="category"><?php $item['category_name'] ?></div>
+                                <img src="<?php $item['image']['url']; ?>" alt="<?php $item['image']['alt']; ?>"/>
+                                <div class="overlay-content">
+                                    <h6 class="overlay-title"><?php $item['project_name']; ?></h6>
+                                    <div class="overlay-icons">
+                                        <a href="<?php $item['image']['url']; ?>" data-lity>
+                                            <span class="icon-eye"></span>
+                                        </a>
+                                        <a href="<?php $item['view_project_link']; ?>">
+                                            <span class="icon-link"></span>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+					<?php } ?>
 				</div>
 			</div>
 			<!--portfolio list ends!-->
